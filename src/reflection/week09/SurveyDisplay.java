545cb2bf4d46b516a8e4d9dc800608a03ecb28c0
tina-fernan <tinaaroom@gmail.com>
2019-01-18 13:38:55 +0100

    examWeek09

--- src/reflection/week09/SurveyDisplay.java
+++ src/reflection/week09/SurveyDisplay.java
@@ -4,55 +4,60 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SurveyDisplay {
-    //How many boys and girls are there?
-    //Which are the distinct parental levels of education sorted alphabetically?
-    //How many students scored higher than a 90 on every topic?
-    //How many students scored exactly 100 on every topic? Which ones are their genders?
+
 
     List<Performances>performances=new performanceServuy().getPerformances();
+
 
     public void surveyStudents(){
         getGender();
         getParental();
-        getHundered();
         getEveryTopic();
+        getHundered();
     }
 
     private void getGender(){
 
-        List<Integer> collect = performances.stream()
-                .map(e -> e.getGender().length())
-                .collect(Collectors.toList());
-        System.out.println("The number of boys and girls :");
-        System.out.println(collect);
+        long collect = performances.stream()
+                .filter(e->e.getGender().contains("femal"))
+                .count();
+        long male = performances.size() - collect;
+
+        System.out.println("Total number of student performance entries: " + performances.size());
+        System.out.println("Number of female students: " + collect);
+        System.out.println("Number of male students: " + male);
+
     }
 
     private void getParental(){
         List<String> collect = performances.stream()
                 .map(e -> e.getParentalEducation())
                 .distinct()
+                .sorted()
                 .collect(Collectors.toList());
-        System.out.println("Parental level of Education sorted Alphabetically");
+        System.out.println("Parental level of Education sorted Alphabetically : ");
         System.out.println(collect);
     }
 
     private void getEveryTopic(){
-        List<Performances> collect = performances.stream()
+        long collect = performances.stream()
                 .filter(e -> e.getMathScore() > 90)
                 .filter(e -> e.getReadingScore() > 90)
                 .filter(e -> e.getWrikttingScore() > 90)
-                .collect(Collectors.toList());
+                .count();
+
         System.out.println("Students score higher than 90");
         System.out.println(collect);
     }
 
     private void getHundered(){
-        List<String> collect = performances.stream()
+        Long collect = performances.stream()
                 .filter(e -> e.getMathScore() == 100)
                 .filter(e -> e.getWrikttingScore() == 100)
                 .filter(e -> e.getReadingScore() == 100)
                 .map(e -> e.getGender())
-                .collect(Collectors.toList());
+                .count();
+
         System.out.println("Students get 100 in every topics ");
         System.out.println(collect);
     }
